97556ee59fa4191f1e85addc29a85c2986ac201e
tjzetty <tjzetty@mtu.edu>
2022-11-02 15:15:55 -0400

    Starting implementation of hips and waist

--- src/App.js
+++ src/App.js
@@ -43,23 +43,35 @@ function App() {
       console.log(person);
       let bodyParts = person.allPoses; // Different confidence values
       let dataArray = person.data; // Body segmentation on camera
+
+      // Measurements
       let neckMin = [-1, -1];
       let neckMax = [-1, -1];
+      let waistMin = [-1, -1];
+      let waistMax = [-1, -1];
+      let hipskMin = [-1, -1];
+      let hipsMax = [-1, -1];
+
       console.log("data: " + typeof(dataArray) + " , data[]: " + dataArray[0] + ", data[][]" + typeof(dataArray[0][0]));
       for (var i = 0; i < dataArray.length - 640; i++) {
         let current = dataArray[i];
         let next = dataArray[i +640];
-        // TODO: Check to see if current is 1 or 0 (head) and i-1 is a 13 (chest)
+        let x = i % 640;
+        let y = Math.floor(i / 640);
+
         let currentIsHead = current === 0 || current === 1;
+        let currentIsBody = current === 13 || current === 12;
         let belowIsBody = next === 13 || next === 12;
+        let belowIsWaist = next === 15 || next === 16; // TODO: THESE WILL BE CHANGED
+
         if (currentIsHead && belowIsBody) {
-          let x = i % 640;
-          let y = Math.floor(i / 640);
           neckMin[0] = (neckMin[0] < 0) ? x : Math.min(x, neckMin[0]);
           neckMax[0] = (neckMax[0] < 0) ? x : Math.max(x, neckMax[0]);
           neckMin[1] = (neckMin[1] < 0) ? x : Math.min(y, neckMin[1]);
           neckMax[1] = (neckMax[1] < 0) ? x : Math.max(y, neckMax[1]);
           // console.log("x: " + x + ", y: " + y);
+        } else if (currentIsBody && belowIsWaist) {
+
         }
       }
       console.log("Neck width: ", neckMax[0] - neckMin[0]);
