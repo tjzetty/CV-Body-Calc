98d40c9a8b7de4eca373946c38a2c86b55f44651
OGKjeeze <kjeezekevinkj@gmail.com>
2022-11-07 20:13:53 -0500

    test push

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import * as tf from "@tensorflow/tfjs";
 import * as bodyPix from "@tensorflow-models/body-pix";
 
 import "./App.css";
+import { conv2d, image, model, Tensor, tensor6d } from "@tensorflow/tfjs";
 
+let timerCount = 3;
+let holdStill = 1;
 function App() {
+  let imageHolder = null;
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
 
@@ -40,10 +44,50 @@ function App() {
         internalResolution: "medium",
         segmentationThreshold: 0.7,
       });
-      console.log(person);
+
+      //console.log(person);
+
       let bodyParts = person.allPoses; // Different confidence values
+      //console.log(person.allPoses);
+      let total = 0;
+      for(let count = 0; count < bodyParts.length; count++)//gets average of different scores 
+      {
+          let hold = bodyParts[count]['keypoints'];
+          let tempTot = 0;
+          for(let count2 = 0; count2 < 17; count2++)//averages the keypoints
+          {
+              if(count2 == 0 || count2 == 1 || count2 == 2 || count2 == 5 || count2 == 6 || count2 > 10)
+              {
+                tempTot += hold[count2]["score"];
+              }
+          }
+          tempTot = tempTot/11;
+          total += tempTot;
+      }
+
+      total = total/bodyParts.length;
+
+      if(total > 0.9 && holdStill == 1)//checks if confidence is above %90
+      {
+        setInterval(countDown,1000);
+        console.log(timerCount);
+        holdStill = 0;
+      }
+      else
+      {
+        //holdStill = 1;
+        timerCount = 3;
+      }
+
+      if(timerCount <= 0)//when timer set to 0 save image
+      {
+        imageHolder = await net.estimatePersonSegmentation(video, 16, 0.7); //save image I think
+        console.log(imageHolder);
+      }
+      //console.log(total/bodyParts.length);
+
       let dataArray = person.data; // Body segmentation on camera
-      console.log([...new Set(dataArray)]); // Different numbers for different body parts.
+      //console.log([...new Set(dataArray)]); // Different numbers for different body parts.
 
 
       // Draw detections
@@ -98,4 +142,8 @@ function App() {
   );
 }
 
+function countDown()
+{
+  timerCount--;
+}
 export default App;
