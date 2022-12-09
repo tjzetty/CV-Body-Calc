024668f0f5ba5abe91b402c3c24a0f53aa4be477
tjzetty <tjzetty@mtu.edu>
2022-12-08 20:24:42 -0500

    Adding firebase project to app

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import React, { useRef, useState } from "react";
 import Webcam from "react-webcam";
 import { tf } from "@tensorflow/tfjs";  // eslint-disable-line no-unused-vars
 import * as bodyPix from "@tensorflow-models/body-pix";
-
+import { initializeApp } from "firebase/app";
 import "./App.css";
+
+const app = initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+});
+
 
 const appState = {
   userData : "input your data",
