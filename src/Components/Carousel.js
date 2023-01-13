4140d554c2df892ec10871aa4800f943df88ca15
tjzetty <tjzetty@mtu.edu>
2023-01-13 15:30:32 -0500

    Manage Images and Public Folder

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -19,13 +19,11 @@ const images = [
   },
   {
     label: "Take a front-profile photo",
-    imgPath:
-      "https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60",
+    imgPath: process.env.PUBLIC_URL + "/carousel-images/tose.png",
   },
   {
     label: "Take a side-profile photo",
-    imgPath:
-      "https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250",
+    imgPath: process.env.PUBLIC_URL + "/carousel-images/side-tose.png",
   },
   {
     label: "Get your results!",
